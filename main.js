1406c5e4de2b698247d38beaa5b228a16b576f99
shrackw <114085993+shrackw@users.noreply.github.com>
2022-12-22 22:03:43 +0200

    main

---
+++ main.js
@@ -0,0 +1,42 @@
+const {Collection, Intents, Client} = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES,Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
+const fs = require('fs');
+const token = require('./config.json');
+
+client.on('ready', () => {
+    console.log(`Logged in as user: ${client.user.tag}, ID: ${client.user.id}`);
+    client.user.setStatus('online');
+    client.user.setActivity("NOXWORD", {
+        type: "PLAYING",
+      });
+    });
+
+client.on('messageCreate', msg => {
+    if (!msg.channel.guild) {
+      return;
+    }
+  });
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+      const event = require(`./events/${file}`);
+      let eventName = file.split(".")[0];
+      console.log(`Loading event ${eventName}`);
+      client.on(eventName, event.bind(null, client));
+    });
+});
+
+client.commands = new Collection();
+
+fs.readdir("./commands/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file => {
+        if (!file.endsWith(".js")) return;
+        let props = require(`./commands/${file}`);
+        let commandName = file.split(".")[0];
+        console.log(`Loaded ${commandName}`);
+        client.commands.set(commandName, props);
+    });
+});
+client.login(token.token);
